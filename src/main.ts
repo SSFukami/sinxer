84815b8026ea6daaad17d553437472ef422ee17d
ShusakuFukami <beet.shudora@docomo.ne.jp>
2021-01-15 22:17:03 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSWfTjgJeN_xDNY3XAl9TUrnBR-N1vsvA",
+  authDomain: "sinxer-49d2b.firebaseapp.com",
+  projectId: "sinxer-49d2b",
+  storageBucket: "sinxer-49d2b.appspot.com",
+  messagingSenderId: "228946429745",
+  appId: "1:228946429745:web:b0fcdbbe39902057fed210",
+  measurementId: "G-GF1PSMQZLW"
+};
+firebase.initializeApp(firebaseConfig);
+createApp(App).use(store).use(router).mount('#app')
