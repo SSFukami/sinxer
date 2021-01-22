3ee961b6e147be0bb44d6de08c5baebf14192838
sinowin <sinowin4020@gamil.com>
2021-01-22 18:47:26 +0900

    ホーム画面の枠組み完成

---
+++ src/components/organisms/HomeTile.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="home-tile">
+    <div class="icon">
+      <div class="icon-content"></div>
+    </div>
+    <div class="name">
+      <div class="name-content">お名前{{ id }}</div>
+    </div>
+    <div class="detail">紹介文</div>
+    <div class="fee">料金</div>
+    <div class="deadline">納期</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeTile",
+  props: {
+    id: Number,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.home-tile {
+  min-width: 320px;
+  max-width: 400px;
+  height: 280px;
+  display: grid;
+  grid-template:
+    "icon   name   name   name  " 64px
+    "detail detail detail detail" 1fr
+    "fee    fee    line   line  " 48px
+    / 1fr 1fr 1fr 1fr;
+  column-gap: 8px;
+  row-gap: 8px;
+  border: 2px solid gray;
+  margin: 0 auto; //左右中央揃え用
+  padding: 8px;
+  cursor: pointer;
+
+  .icon {
+    grid-area: icon;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-content {
+      width: 60px;
+      height: 60px;
+      background: silver;
+      border-radius: 50%;
+    }
+  }
+  .name {
+    grid-area: name;
+    border-left: 4px solid silver;
+    border-right: 4px solid silver;
+    &-content {
+      width: 100%;
+      height: 100%;
+      font-size: 32px;
+      color: silver;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+  .detail {
+    grid-area: detail;
+    background: silver;
+    border-bottom: 2px dashed silver;
+  }
+  .fee {
+    grid-area: fee;
+    background: silver;
+  }
+  .deadline {
+    grid-area: line;
+    background: silver;
+  }
+}
+</style>
