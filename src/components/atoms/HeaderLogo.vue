05a6846b487b87e2b7f2f99179a8a8d4d43270af
sinowin <sinowin4020@gamil.com>
2021-02-18 20:41:24 +0900

    ヘッダーロゴにホーム画面遷移処理追加

--- src/components/atoms/HeaderLogo.vue
+++ src/components/atoms/HeaderLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header-logo">Sinxer</div>
+  <div class="header-logo" @click="transHome">Sinxer</div>
 </template>
 
 <script lang="ts">
@@ -7,6 +7,11 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HeaderLogo",
+  methods: {
+    transHome(): void {
+      this.$router.push("/");
+    },
+  },
 });
 </script>
 
@@ -15,5 +20,6 @@ export default defineComponent({
 .header-logo {
   font-size: 24px;
   color: $-primary-100;
+  cursor: pointer;
 }
 </style>
