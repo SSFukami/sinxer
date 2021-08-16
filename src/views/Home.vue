2e83392bf7cfa1fd1533111c592f7b2dcc0b2992
sinowin <sinowin4020@gamil.com>
2021-08-16 20:55:00 +0900

    home-content の下の空白修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,13 +16,15 @@
       </div>
     </div>
     <div class="home-content">
-      <HomeTile
-        v-for="(data, index) in homeMixerList"
-        :key="index"
-        :data="data"
-        :icon="iconList[index]"
-        @click="setMixerIcon(iconList[index])"
-      />
+      <div class="home-content-wrapper">
+        <HomeTile
+          v-for="(data, index) in homeMixerList"
+          :key="index"
+          :data="data"
+          :icon="iconList[index]"
+          @click="setMixerIcon(iconList[index])"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -71,6 +73,9 @@ export default defineComponent({
     iconList(): string[] {
       //ホームに表示するMix師のアイコンのリスト
       return (this as any).$store.state.trimming.iconList;
+    },
+    windowWidth(): number {
+      return window.innerWidth;
     },
   },
   methods: {
@@ -122,18 +127,14 @@ export default defineComponent({
     height: calc(100% - 48px); //home-headerの分引く
     background-color: $-primary-300;
     padding: 50px;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-    column-gap: 20px;
-    row-gap: 20px;
     overflow-y: scroll;
-  }
-  .home-content::after {
-    //画面下の空白のための文字列
-    content: "要素の直後にコンテンツを追加";
-    width: 100%;
-    height: 50px;
-    visibility: hidden;
+    &-wrapper {
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+      column-gap: 20px;
+      row-gap: 20px;
+    }
   }
 }
 
@@ -155,7 +156,7 @@ export default defineComponent({
   }
 
   .home-content {
-    height: calc(100% - 88px); //home-headerの分引く
+    height: calc(100% - 88px) !important; //home-headerの分引く
   }
 }
 </style>
